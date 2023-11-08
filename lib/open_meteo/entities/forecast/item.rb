4c31546bfa25e36c9e68681517b46fbe9dbec293
Peter Morgenstern <mail@petergundel.de>
2023-11-08 11:57:36 +0100

    Add Item#weathercode_symbol
    Because sometimes it's more convenient to work with symbols.

--- lib/open_meteo/entities/forecast/item.rb
+++ lib/open_meteo/entities/forecast/item.rb
@@ -4,6 +4,45 @@ module OpenMeteo
       ##
       # A forecast entity for a data item returned by OpenMeteo
       class Item
+        class UnknownWeathercode < StandardError
+        end
+
+        ##
+        # The conversion map of weathercodes to a more descriptive symbol.
+        #
+        # See "WMO Weather interpretation codes (WW)" on
+        # https://open-meteo.com/en/docs
+        WMO_CODE_TO_SYMBOL_MAP = {
+          0 => :clear_sky,
+          1 => :mainly_clear,
+          2 => :partly_cloudy,
+          3 => :overcast,
+          45 => :fog,
+          48 => :depositing_rime_fog,
+          51 => :drizzle_light,
+          53 => :drizzle_moderate,
+          55 => :drizzle_dense,
+          56 => :freezing_drizzle_light,
+          57 => :freezing_drizzle_dense,
+          61 => :rain_slight,
+          63 => :rain_moderate,
+          65 => :rain_heavy,
+          66 => :freezing_rain_slight,
+          67 => :freezing_rain_heavy,
+          71 => :snow_slight,
+          73 => :snow_moderate,
+          75 => :snow_heavy,
+          77 => :snow_grains,
+          80 => :rain_showers_slight,
+          81 => :rain_showers_moderate,
+          82 => :rain_showers_violent,
+          85 => :snow_showers_slight,
+          86 => :snow_showers_heavy,
+          95 => :thunderstorm_slight_or_moderate,
+          96 => :thunderstorm_with_slight_hail,
+          99 => :thunderstorm_with_heavy_hail,
+        }.freeze
+
         attr_reader :raw_json, :attributes, :time
 
         def initialize(json_data)
@@ -11,6 +50,16 @@ module OpenMeteo
 
           @attributes = json_data.keys
           @time = json_data["time"]
+        end
+
+        ##
+        # Provide a symbol for the weathercode.
+        #
+        # @see WMO_CODE_TO_SYMBOL_MAP
+        def weathercode_symbol
+          return if weathercode.nil?
+
+          WMO_CODE_TO_SYMBOL_MAP.fetch(weathercode) { raise UnknownWeathercode, weathercode }
         end
 
         private
