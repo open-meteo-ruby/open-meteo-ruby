0ce3d9947298b5d877ac770694173bae390bba55
Caleb Buxton <me@cpb.ca>
2024-10-21 07:22:52 -0700

    🚨 prettier

--- spec/open_meteo/integration/configuration/logging_spec.rb
+++ spec/open_meteo/integration/configuration/logging_spec.rb
@@ -2,13 +2,11 @@ RSpec.describe "Integration > configuration > logging" do
   subject(:response) { forecast.get(location:, variables:) }
 
   let(:logger_stream) { StringIO.new }
-  let(:config) { OpenMeteo::Client::Config.new(logger: Logger.new(logger_stream))}
+  let(:config) { OpenMeteo::Client::Config.new(logger: Logger.new(logger_stream)) }
   let(:client) { OpenMeteo::Client.new(config:) }
   let(:forecast) { OpenMeteo::Forecast.new(client:) }
 
-  let(:location) do
-    OpenMeteo::Entities::Location.new(latitude: 52.52.to_d, longitude: 13.41.to_d)
-  end
+  let(:location) { OpenMeteo::Entities::Location.new(latitude: 52.52.to_d, longitude: 13.41.to_d) }
 
   let(:variables) do
     {
@@ -20,9 +18,8 @@ RSpec.describe "Integration > configuration > logging" do
   end
 
   it "writes to the configured logger when making requests" do
-    expect { VCR.use_cassette("integration/forecast/general") { response } }
-      .to change { logger_stream.tap(&:rewind).read }
-      .from(be_empty)
-      .to(include("Faraday"))
+    expect { VCR.use_cassette("integration/forecast/general") { response } }.to change {
+      logger_stream.tap(&:rewind).read
+    }.from(be_empty).to(include("Faraday"))
   end
 end
