fa878ce75ca933e5a707b9bc370ef5bad62167fa
Johann Speer <johann.speer@proton.me>
2024-11-16 18:22:16 -0500

    Added support for the geolocation API. Including a search class and search results to deserialize the results.

---
+++ lib/open_meteo/search/variables.rb
@@ -0,0 +1,17 @@
+module OpenMeteo
+  class Search
+    ##
+    # The variables used for a Search in the Open Mateo Geolocation API
+    #
+    class Variables < Dry::Struct
+      attribute(:count, OpenMeteo::Types::Strict::Integer.optional.default(nil))
+      attribute(:language, OpenMeteo::Types::Strict::String.optional.default(nil))
+
+      def to_query_params
+        %i[count language].each_with_object({}) do |key, query_params|
+          query_params[key] = send(key) if send(key)
+        end
+      end
+    end
+  end
+end
